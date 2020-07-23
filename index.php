cb3e7550c9bc9f1f74aeb5c78b699fa8dd3ca575
adimael <adimaelbr@gmail.com>
2020-07-23 12:33:20 -0300

    ajutes de layout da tela de LOGIN

--- index.php
+++ index.php
@@ -1,72 +1,197 @@
 <?php session_start(); ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
+
 <head>
-	<title>Fincorp CRM - Controle de Estoque e venda.</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
-	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="assets/css/toast.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Fincorp</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
+		<meta name="msapplication-tap-highlight" content="no">
+		 <!-- Favicon -->
+		 <link rel="icon" href="favicon.png" type="assets/images/crm.png" />
+  <link rel="shortcut icon" href="assets/images/favicon.ico" />
+    <!--
+    =========================================================
+    * ArchitectUI HTML Theme Dashboard - v1.0.0
+    =========================================================
+    * Product Page: https://dashboardpack.com
+    * Copyright 2019 DashboardPack (https://dashboardpack.com)
+    * Licensed under MIT (https://github.com/DashboardPack/architectui-html-theme-free/blob/master/LICENSE)
+    =========================================================
+    * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+		-->
+		<!-- SweetAlert2 -->
+		<link rel="stylesheet" href="assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <!-- Toastr -->
+  <link rel="stylesheet" href="assets/plugins/toastr/toastr.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/toast.css">
+		<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+		<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+    <link href="assets/css/darkmode.css" rel="stylesheet">
     <link href="assets/css/main.css" rel="stylesheet">
-<link href="assets/css/scrollbar.css" rel="stylesheet">
-	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="assets/css/scrollbar.css" rel="stylesheet">
 </head>
 <body>
-<div class="app-header header-shadow">
-<div class="app-header__logo">
-<div class="logo-titulo"><img class="img-logo" src="assets/images/crm.png">Fincorp</div>
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+    <div class="app-header header-shadow">
+    <div class="app-header__logo">
+    <div class="logo-src"></div>
+    <div class="header__pane ml-auto">
+    <div>
+        <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+        </button>
+    </div>
+</div>
 </div>
 <div class="app-header__mobile-menu">
 <div>
-<button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
-<span class="hamburger-box">
-<span class="hamburger-inner"></span>
-</span>
-</button>
+    <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+        <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+        </span>
+    </button>
 </div>
 </div>
 <div class="app-header__menu">
 <span>
-<button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-<span class="btn-icon-wrapper">
-<i class="fa fa-ellipsis-v fa-w-6" aria-hidden="true"></i>
+    <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+        <span class="btn-icon-wrapper">
+            <i class="fa fa-ellipsis-v fa-w-6"></i>
+        </span>
+    </button>
 </span>
-</button>
-</span>
-</div>    
-<div class="app-header__content">
+</div>    <div class="app-header__content">
 <div class="app-header-left">
-<div class="search-wrapper">
-<div class="input-holder">
-<input type="text" class="search-input" placeholder="Procurar...">
-<button class="search-icon"><span></span></button>
-</div>
-<button class="close"></button>
-</div>
-<ul class="header-menu nav">
-<li class="nav-item">
-<a href="/fincorp/index.php" class="nav-link">
-<i class="nav-link-icon fa fa-home" aria-hidden="true"> </i>
-Inicio
-</a>
-</li>
-<li class="btn-group nav-item">
-<a href="javascript:void(0);" class="nav-link" data-toggle="modal" data-target=".bd-example-modal-lg-produto">
-<i class="nav-link-icon fa fa-blog" aria-hidden="true"></i>
-Blog
-</a>
-</li>
-<li class="dropdown nav-item">
-<a href="javascript:void(0);" class="nav-link">
-<i class="nav-link-icon fa fa-info" aria-hidden="true"></i>
-Suporte
-</a>
-</li>
-</ul>   
-     </div>
-</div>
-</div>
-	<div class="container">
+    <div class="search-wrapper">
+        <div class="input-holder">
+            <input type="text" class="search-input" placeholder="Procurar...">
+            <button class="search-icon"><span></span></button>
+        </div>
+        <button class="close"></button>
+    </div>
+    <ul class="header-menu nav">
+		<li class="nav-item">
+			<a href="/fincorp/index.php" class="nav-link">
+			<i class="nav-link-icon fa fa-home" aria-hidden="true"> </i>
+				Inicio
+			</a>
+		</li>
+		<li class="btn-group nav-item">
+			<a href="javascript:void(0);" class="nav-link" data-toggle="modal" data-target=".bd-example-modal-lg-produto">
+			<i class="nav-link-icon fa fa-blog" aria-hidden="true"></i>
+				Blog
+			</a>
+		</li>
+		<li class="dropdown nav-item">
+			<a href="javascript:void(0);" class="nav-link">
+			<i class="nav-link-icon fa fa-info" aria-hidden="true"></i>
+				Suporte
+			</a>
+		</li>
+    </ul>        </div>
+    <div class="app-header-right">
+    <div class="header-btn-lg pr-0">
+    <div class="widget-content p-0">
+    <div class="widget-content-wrapper">
+    <div class="widget-content-left">
+    </div>
+    <div class="widget-content-right header-user-info ml-3">
+    </div>
+    </div>
+    </div>
+    </div>        </div>
+    </div>
+    </div>        <div class="ui-theme-settings">
+    <div class="theme-settings__inner">
+    <div class="scrollbar-container">
+    </div>
+    </div>
+    </div>        <div class="app-main">
+    <div class="app-sidebar sidebar-shadow">
+    <div class="app-header__logo">
+    <div class="logo-src"></div>
+    <div class="header__pane ml-auto">
+    <div>
+    <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+        <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+        </span>
+    </button>
+    </div>
+    </div>
+    </div>
+    <div class="app-header__mobile-menu">
+    <div>
+    <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+        <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+        </span>
+    </button>
+    </div>
+    </div>
+    <div class="app-header__menu">
+    <span>
+    <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+        <span class="btn-icon-wrapper">
+            <i class="fa fa-ellipsis-v fa-w-6"></i>
+        </span>
+    </button>
+    </span>
+    </div>    <div class="scrollbar-sidebar">
+    <div class="app-sidebar__inner">
+    <ul class="vertical-nav-menu">
+    <li class="app-sidebar__heading">Home-Inicial</li>
+    <li>
+        <a href="dashboard.php" class="mm-active">
+            <i class="metismenu-icon pe-7s-home"></i>
+            Página principal
+        </a>
+    </li>
+		<li class="app-sidebar__heading">Menu do Cliente</li>
+		<li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-user">
+            </i>Painel do cliente
+        </a>
+    </li>
+    <li>
+        <a href="forms-layouts.html">
+            <i class="metismenu-icon pe-7s-cart">
+            </i>Loja Virtual
+        </a>
+    </li>
+    <li>
+        <a href="forms-validation.html">
+            <i class="metismenu-icon pe-7s-news-paper">
+            </i>Blog
+        </a>
+    </li>
+    <li class="app-sidebar__heading">Suporte</li>
+    <li>
+        <a href="">
+            <i class="metismenu-icon pe-7s-info">
+            </i>Central de Suporte
+        </a>
+    </li>
+</ul>
+    </div>
+    </div>
+    </div>    <div class="app-main__outer">
+    <div class="app-main__inner">
+    <div class="app-page-title">
+    <div class="page-title-wrapper">
+    <div class="page-title-heading">
+            </div>
+        </div>
+		</div>
+		<div class="container">
 		<div class="login-content">
 			<form action="login.php" method="post">
 				<img src="assets/images/crm.png">
@@ -77,7 +202,7 @@ Suporte
            		   </div>
            		   <div class="div">
            		   		<h5>Username</h5>
-           		   		<input type="text" name="username" class="input">
+           		   		<input type="text" spellcheck="false" name="username" class="input">
            		   </div>
            		</div>
            		<div class="input-div pass">
@@ -95,52 +220,45 @@ Suporte
             </form>
         </div>
     </div>
-		<script type="text/javascript" src="assets/js/index.js"></script>
-		<script type="text/javascript" src="assets/js/main.js"></script>
-</body>
-</html>
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Fincorp CRM - Controle de Estoque e venda.</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
-	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="assets/css/toast.css">
-	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-	<div class="container">
-		<div class="login-content">
-			<form action="login.php" method="post">
-				<img src="assets/images/avatar.png">
-				<h2 class="title">Fincorp</h2>
-           		<div class="input-div one">
-           		   <div class="i">
-           		   		<i class="fas fa-user"></i>
-           		   </div>
-           		   <div class="div">
-           		   		<h5>Username</h5>
-           		   		<input type="text" name="username" class="input">
-           		   </div>
-           		</div>
-           		<div class="input-div pass">
-           		   <div class="i"> 
-           		    	<i class="fas fa-lock"></i>
-           		   </div>
-           		   <div class="div">
-           		    	<h5>Password</h5>
-           		    	<input type="password" name="password" class="input">
-            	   </div>
-            	</div>
-            	<a href="#">Esqueceu a senha?</a>
-							<input type="submit" class="btn" value="Login">
-							<?php include("error.php") ?>
-            </form>
+						</div>
+						<div class="app-wrapper-footer">
+                        <div class="app-footer">
+                            <div class="app-footer__inner">
+                                <div class="app-footer-left">
+                                    <ul class="nav">
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="nav-link">
+																						Copyright © 2020-2021 Fincorp. All rights reserved.
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="app-footer-right">
+                                    <ul class="nav">
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="nav-link">
+                                                <div class="badge badge-success mr-1 ml-0">
+                                                </div>
+                                                Version 1.0.0
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    </div>
+</div>            
+</div>
+</div>
+</div>
         </div>
     </div>
-		<script type="text/javascript" src="assets/js/index.js"></script>
-		<script type="text/javascript" src="assets/js/main.js"></script>
+				<script type="text/javascript" src="assets/js/darkmode.js"></script>
+				<script type="text/javascript" src="assets/js/index.js"></script>
+<script type="text/javascript" src="assets/js/main.js"></script>
+				<!-- SweetAlert2 -->
+<script src="../assets/plugins/sweetalert2/sweetalert2.min.js"></script>
+<!-- Toastr -->
+<script src="../assets/plugins/toastr/toastr.min.js"></script>
 </body>
 </html>
---
+++ w/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Fincorp</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <!-- Favicon -->
+  <link rel="icon" href="favicon.png" type="../assets/images/crm.png" />
+  <link rel="shortcut icon" href="../assets/images/favicon.ico" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>404 Error Página</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+              <li class="breadcrumb-item active">404 Error Page</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="error-page">
+        <h2 class="headline text-warning"> 404</h2>
+
+        <div class="error-content">
+          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Opa! Página não encontrada.</h3>
+
+          <p>
+          Não foi possível encontrar a página que você estava procurando.
+          Enquanto isso, você pode <a href="dashboard.php">retornar ao painel</a>.
+          </p>
+          </form>
+        </div>
+        <!-- /.error-content -->
+      </div>
+      <!-- /.error-page -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2020-2021 <a href="javascript:void(0);">Fincorp</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../assets/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../assets/dist/js/demo.js"></script>
+</body>
+</html>
